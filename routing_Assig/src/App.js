65bc1950c8e42406caff224fdcf1bdc406fa08a5
unknown <awatefhajji5@gmail.com>
2023-03-14 14:37:43 +0100

    Routing_Passig

---
+++ routing_Assig/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Number from './components/Number';
+// import Hello from './components/Hello';
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:numb/:bcolor/:color" element={<Number />} />
+        {/* <Route path="/hello" element={<Hello />} /> */}
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
